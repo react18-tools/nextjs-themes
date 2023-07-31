e329df6c9b9a06cd4fd625fd7582f42e795468d5
Mayank Kumar Chaudhari <mayank.srmu@gmail.com>
2023-07-31 11:02:38 +0530

    rename packages

--- examples/docs/app/page.tsx
+++ examples/docs/app/page.tsx
@@ -1,9 +1,9 @@
-import { Button, Header } from "ui";
+import { Button, Header } from "nextjs-themes";
 
 export default function Page() {
   return (
     <>
-      <Header text="Web" />
+      <Header text="Docs" />
       <Button />
     </>
   );
--- examples/web/app/page.tsx
+++ examples/web/app/page.tsx
@@ -1,9 +1,9 @@
-import { Button, Header } from "ui";
+import { Button, Header } from "nextjs-themes";
 
 export default function Page() {
   return (
     <>
-      <Header text="Docs" />
+      <Header text="Web" />
       <Button />
     </>
   );
