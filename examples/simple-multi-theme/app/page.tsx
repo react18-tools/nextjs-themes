0cddd162bd31fcb38e05d92fe98e3872a03ba29c
Mayank Kumar Chaudhari <mayank.srmu@gmail.com>
2023-08-04 09:38:14 +0530

    simple multi theme example

--- examples/simple-multi-theme/app/page.tsx
+++ examples/simple-multi-theme/app/page.tsx
@@ -1,8 +1,14 @@
+import ThemeSelector from "./ThemeSelector";
+
 export default function Page() {
   return (
-    <>
-      <h1>Hello Docs</h1>
-      <p>Hi!</p>
-    </>
+    <div className="container">
+      <h1>Simple Multi Theme</h1>
+      <hr />
+      <p>
+        Example showing how to use <code>nextjs-themes</code> to implement simple multi theme switching
+      </p>
+      <ThemeSelector />
+    </div>
   );
 }
