c0828f39e808263b584b6eba9327e72a2804aa2b
Mayank Kumar Chaudhari <mayank.srmu@gmail.com>
2023-08-04 08:23:23 +0530

    create example placeholders

---
+++ examples/advanced-multi-theme/app/layout.tsx
@@ -0,0 +1,12 @@
+import { ThemeSwitcher } from "nextjs-themes";
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        <ThemeSwitcher />
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ examples/simple-multi-theme/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <>
+      <h1>Hello Docs</h1>
+      <p>Hi!</p>
+    </>
+  );
+}
