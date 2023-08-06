95d7e71f91a7fd8fa8df1a23db26a09deb6c29cb
Mayank Kumar Chaudhari <mayank.srmu@gmail.com>
2023-08-06 18:18:22 +0530

    Add pages router example
    fixes #2

---
+++ examples/nextjs-pages-router/components/ColorSchemePreference.tsx
@@ -0,0 +1,20 @@
+"use client";
+import { useTheme, ColorSchemeType } from "nextjs-themes";
+
+const colorSchemes: ColorSchemeType[] = ["", "system", "light", "dark"];
+
+export default function ColorSchemePreference() {
+  const [colorSchemePref, setColorSchemePref] = useTheme(state => [state.colorSchemePref, state.setColorSchemePref]);
+  return (
+    <p>
+      Color Scheme Preference{" "}
+      <select value={colorSchemePref} onChange={e => setColorSchemePref(e.target.value as ColorSchemeType)}>
+        {colorSchemes.map(theme => (
+          <option key={theme} value={theme}>
+            {theme}
+          </option>
+        ))}
+      </select>
+    </p>
+  );
+}
