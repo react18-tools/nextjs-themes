3fd6b68c4b0ce1468080733c98f4445ae468d2f4
Mayank Kumar Chaudhari <mayank.srmu@gmail.com>
2023-08-13 17:31:35 +0530

    add test action and update publish scripts

--- packages/nextjs-themes/createPackageJSON.js
+++ packages/nextjs-themes/createPackageJSON.js
@@ -11,3 +11,11 @@ packageJson.main = "index.js";
 packageJson.types = "index.d.ts";
 
 fs.writeFileSync(path.resolve(__dirname, "dist", "package.json"), JSON.stringify(packageJson, null, 2));
+
+const readMePath = path.resolve(__dirname, "dist", "README.md");
+let readMe = fs.readFileSync(readMePath, { encoding: "utf8" });
+readMe.replace(
+  "[![Publish to npm and GitHub](https://github.com/mayank1513/nextjs-themes/actions/workflows/publish-to-npm-on-new-release.yml/badge.svg)](https://github.com/mayank1513/nextjs-themes/actions/workflows/publish-to-npm-on-new-release.yml)",
+  "",
+);
+fs.writeFileSync(readMePath, readMe);
