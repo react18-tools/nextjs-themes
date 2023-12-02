147cf9e3dbd14526e10def0af8f9d2548d1c3710
Mayank Kumar Chaudhari <mayank.srmu@gmail.com>
2023-12-02 20:23:16 +0530

    add NextJsServerTarget

--- packages/nextjs-themes/src/server/nextjs/server-side-wrapper/server-side-wrapper.tsx
+++ packages/nextjs-themes/src/server/nextjs/server-side-wrapper/server-side-wrapper.tsx
@@ -1,57 +1,89 @@
+import * as React from "react";
 import type { HTMLProps, ReactNode } from "react";
 import { cookies, headers } from "next/headers";
-import * as React from "react";
-import type { ColorSchemeType } from "../../../store";
+import type { ColorSchemeType, ThemeStoreType } from "../../../store";
+import { resolveThemeFromColorScheme } from "../../../utils";
 
-export type ForcedPage = [
-	pathMatcher: RegExp | string,
-	themes: { theme?: string; colorScheme?: ColorSchemeType },
-];
+export type ForcedPage = [pathMatcher: RegExp | string, themes: { theme?: string; colorScheme?: ColorSchemeType }];
 
-type ServerSideWrapperProps = {
-	children: ReactNode;
-	tag?: keyof JSX.IntrinsicElements;
-	forcedPages?: ForcedPage[];
-} & HTMLProps<HTMLElement>;
+export interface NextJsSSRThemeSwitcherProps extends HTMLProps<HTMLElement> {
+  children?: ReactNode;
+  /** @defaultValue 'div' */
+  tag?: keyof JSX.IntrinsicElements;
+  forcedPages?: ForcedPage[];
+}
 
+function sharedServerComponentRenderer(
+  { children, tag, forcedPages, ...props }: NextJsSSRThemeSwitcherProps,
+  defaultTag: "div" | "html",
+) {
+  const Tag: keyof JSX.IntrinsicElements = tag || defaultTag;
+  const state = cookies().get("react18-themes")?.value;
+
+  const path = headers().get("referer");
+  const forcedPageData = forcedPages?.find(forcedPage => path?.match(forcedPage[0]));
+
+  const themeState = state ? (JSON.parse(state) as ThemeStoreType) : undefined;
+  const isSystemDark = cookies().get("data-color-scheme-system")?.value === "dark";
+  const { dataTheme, dataColorScheme } =
+    forcedPageData === undefined
+      ? getTheme(themeState, isSystemDark)
+      : getForcedPageTheme(themeState, forcedPageData, isSystemDark);
+
+  const dataProps: { "data-theme"?: string; "data-color-scheme"?: ColorSchemeType } = {};
+  if (dataTheme !== undefined) dataProps["data-theme"] = dataTheme;
+  if (dataColorScheme !== undefined) dataProps["data-color-scheme"] = dataColorScheme;
+
+  return (
+    // @ts-expect-error -> svg props and html element props conflict
+    <Tag id="react18-themes" {...dataProps} {...props} data-testid="server-side-target">
+      {children}
+    </Tag>
+  );
+}
+
+export function NextJsSSGThemeSwitcher(props: NextJsSSRThemeSwitcherProps) {
+  return sharedServerComponentRenderer(props, "div");
+}
+
+/** For naming consistancy, clarity, and minimizing API updates */
+export { NextJsSSGThemeSwitcher as NextJsServerTarget };
+
+export interface ServerSideWrapperProps extends NextJsSSRThemeSwitcherProps {
+  /** @defaultValue 'html' */
+  tag?: keyof JSX.IntrinsicElements;
+}
 /**
- * # ServerSideWrapper
  * Server side wrapper for Next.js to replace &#x60;html&#x60; tag
  */
-export function ServerSideWrapper({
-	children,
-	tag,
-	forcedPages,
-	...props
-}: ServerSideWrapperProps) {
-	const Tag: keyof JSX.IntrinsicElements = tag || "html";
-	const dataTheme = cookies().get("data-theme")?.value || "";
-	const path = headers().get("x-invoke-path");
-	let theme;
-	if (forcedPages) {
-		const dataThemeDark = cookies().get("data-theme-dark")?.value || "";
-		const dataThemeLight = cookies().get("data-theme-light")?.value || "";
-		const dataColorScheme = cookies().get("data-color-scheme")?.value || "";
-		for (const f of forcedPages) {
-			if (path?.match(f[0])) {
-				if (f[1].theme) {
-					theme = f[1].theme;
-				} else if (f[1].colorScheme === "") {
-					theme = dataTheme;
-				} else {
-					const colorScheme = f[1].colorScheme === "system" ? dataColorScheme : f[1].colorScheme;
-					theme = colorScheme === "dark" ? dataThemeDark : dataThemeLight;
-				}
-			}
-		}
-	}
-	return (
-		// @ts-expect-error -> svg props and html element props conflict
-		<Tag
-			data-theme={theme === undefined ? dataTheme : theme}
-			{...props}
-			data-testid="server-side-wrapper">
-			{children}
-		</Tag>
-	);
+export function ServerSideWrapper(props: ServerSideWrapperProps) {
+  return sharedServerComponentRenderer(props, "html");
+}
+
+interface Theme {
+  dataTheme?: string;
+  dataColorScheme?: ColorSchemeType;
+}
+
+function getTheme(themeState: ThemeStoreType | undefined, isSystemDark: boolean): Theme {
+  if (!themeState) return {};
+  const dataTheme = resolveThemeFromColorScheme(themeState, isSystemDark);
+  return { dataTheme, dataColorScheme: themeState.colorSchemePref };
+}
+
+function getForcedPageTheme(
+  themeState: ThemeStoreType | undefined,
+  forcedPageData: ForcedPage,
+  isSystemDark: boolean,
+): Theme {
+  const dataColorScheme =
+    forcedPageData[1].colorScheme === undefined ? themeState?.colorSchemePref : forcedPageData[1].colorScheme;
+
+  let dataTheme;
+  if (forcedPageData[1].theme) {
+    dataTheme = forcedPageData[1].theme;
+  } else if (dataColorScheme !== undefined && themeState !== undefined) {
+    dataTheme = resolveThemeFromColorScheme({ ...themeState, colorSchemePref: dataColorScheme }, isSystemDark);
+  }
+  return { dataTheme, dataColorScheme };
 }
