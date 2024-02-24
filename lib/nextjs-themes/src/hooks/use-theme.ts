823043b90a04cd26fc4e2365afaef3dee438eadc
Mayank <mayank.srmu@gmail.com>
2024-02-24 17:05:44 +0000

    useTheme hook for the library users

--- lib/nextjs-themes/src/hooks/use-theme.ts
+++ lib/nextjs-themes/src/hooks/use-theme.ts
@@ -1,6 +1,18 @@
 import useRGS from "r18gs";
-import { DEFAULT_ID, ThemeStoreType, initialState } from "../constants";
+import { ColorSchemeType, DEFAULT_ID, ThemeStoreActionsType, ThemeStoreType, initialState } from "../constants";
 
-export function useTheme(targetId?: string) {
+export function useTheme(targetId?: string): ThemeStoreType & ThemeStoreActionsType {
   const [themeState, setThemeState] = useRGS<ThemeStoreType>(targetId ?? DEFAULT_ID, initialState);
+  return {
+    ...themeState,
+    setTheme: (theme: string) => setThemeState(state => ({ ...state, theme })),
+    setDarkTheme: (darkTheme: string) => setThemeState(state => ({ ...state, darkTheme })),
+    setLightTheme: (lightTheme: string) => setThemeState(state => ({ ...state, lightTheme })),
+    setThemeSet: (themeSet: { darkTheme: string; lightTheme: string }) =>
+      setThemeState(state => ({ ...state, ...themeSet })),
+    setColorSchemePref: (colorSchemePref: ColorSchemeType) => setThemeState(state => ({ ...state, colorSchemePref })),
+    setForcedTheme: (forcedTheme?: string) => setThemeState(state => ({ ...state, forcedTheme })),
+    setForcedColorScheme: (forcedColorScheme?: ColorSchemeType) =>
+      setThemeState(state => ({ ...state, forcedColorScheme })),
+  };
 }
