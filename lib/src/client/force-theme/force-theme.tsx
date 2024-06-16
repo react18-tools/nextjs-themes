6c34815892ab8cbef84835591a47c76f91f1c19b
Mayank <mayank.srmu@gmail.com>
2024-06-16 23:12:43 +0530

    Update props to match the old versions APIs exposed to users -- force-theme

--- lib/src/client/force-theme/force-theme.tsx
+++ lib/src/client/force-theme/force-theme.tsx
@@ -1,25 +1,23 @@
-import { HTMLProps, ReactNode } from "react";
-import styles from "./force-theme.module.scss";
+import { HTMLProps } from "react";
 
 export interface ForceThemeProps extends HTMLProps<HTMLDivElement> {
-	children?: ReactNode;
+  theme: string;
 }
 
 /**
- * 
+ *
  *
  * @example
  * ```tsx
  * <ForceTheme />
  * ```
- * 
+ *
  * @source - Source code
  */
 export const ForceTheme = ({ children, ...props }: ForceThemeProps) => {
-  const className = [props.className, styles["force-theme"]].filter(Boolean).join(" ");
-	return (
-		<div {...props} className={className} data-testid="force-theme">
-			{children}
-		</div>
-	);
-}
+  return (
+    <div {...props} data-testid="force-theme">
+      {children}
+    </div>
+  );
+};
