7feb476be46485cab2bdb0bf4926de84577d14c8
Mayank <mayank.srmu@gmail.com>
2024-06-16 17:51:30 +0530

    Clean up server side wrapper
    - Ensuring no breaking changes.
    Kept here only to ensure that existing app do not start throwing errors.

--- lib/nextjs-themes/src/server/nextjs/server-side-wrapper/server-side-wrapper.tsx
+++ lib/nextjs-themes/src/server/nextjs/server-side-wrapper/server-side-wrapper.tsx
@@ -1,11 +1,7 @@
-import * as React from "react";
 import type { HTMLProps, ReactNode } from "react";
-import { cookies, headers } from "next/headers";
-import { DEFAULT_ID, type ColorSchemeType, type ThemeStoreType } from "../../../constants";
-import { resolveTheme } from "../../../utils";
-import { DataProps, UpdateProps } from "../../../client";
+import { type ColorSchemeType } from "../../../constants";
 
-export type ForcedPage =
+type ForcedPage =
   | { pathMatcher: RegExp | string; props: { forcedTheme?: string; forcedColorScheme?: ColorSchemeType } }
   | [pathMatcher: RegExp | string, themes: { theme?: string; colorScheme?: ColorSchemeType }];
 
@@ -20,72 +16,37 @@ export interface NextJsSSRThemeSwitcherProps extends HTMLProps<HTMLElement> {
   styles?: Record<string, string>;
 }
 
-/** getData props */
-const getDataProps = (resolvedData: UpdateProps, styles?: Record<string, string>) => {
-  const dataProps: DataProps = { className: "" };
-  let classeNames = [];
-  if (resolvedData.resolvedColorScheme !== undefined) {
-    dataProps["data-color-scheme"] = resolvedData.resolvedColorScheme;
-    classeNames.push(resolvedData.resolvedColorScheme);
-  }
-  if (resolvedData.resolvedTheme !== undefined) {
-    dataProps["data-theme"] = resolvedData.resolvedTheme;
-    classeNames.push(`theme-${resolvedData.resolvedTheme}`);
-  }
-  if (resolvedData.th) {
-    dataProps["data-th"] = resolvedData.th;
-    classeNames.push(`th-${resolvedData.th}`);
-  }
-  if (resolvedData.resolvedColorSchemePref !== undefined) {
-    dataProps["data-csp"] = resolvedData.resolvedColorSchemePref;
-    classeNames.push(`csp-${resolvedData.resolvedColorSchemePref}`);
-  }
-  if (styles) classeNames = classeNames.map(cls => styles[cls] ?? cls);
-  dataProps.className = classeNames.join(" ");
-  return dataProps;
-};
-
 /** Shared server component renderer for Next.js SSG and SSR */
 /** @internal */
 const sharedServerComponentRenderer = (
-  { children, tag, forcedPages, targetId, styles, ...props }: NextJsSSRThemeSwitcherProps,
+  { children, tag, ...props }: NextJsSSRThemeSwitcherProps,
   defaultTag: "div" | "html",
 ) => {
   const Tag: keyof JSX.IntrinsicElements = tag || defaultTag;
-  const key = targetId ? `#${targetId}` : DEFAULT_ID;
-  const state = cookies().get(key)?.value;
-
-  const path = headers().get("referer");
-  const forcedPage = forcedPages?.find(forcedPage =>
-    path?.match(Array.isArray(forcedPage) ? forcedPage[0] : forcedPage.pathMatcher),
-  );
-  const forcedPageProps = Array.isArray(forcedPage)
-    ? { forcedTheme: forcedPage[1].theme, forcedColorScheme: forcedPage[1].colorScheme }
-    : forcedPage?.props;
-  const themeState = state ? (JSON.parse(state) as ThemeStoreType) : undefined;
-  const resolvedData = resolveTheme(themeState, forcedPageProps);
-  const dataProps = getDataProps(resolvedData, styles);
-  if (targetId) dataProps.className += styles?.[" nth-scoped"] ?? " nth-scoped";
-
   return (
     // @ts-expect-error -> svg props and html element props conflict
-    <Tag id={targetId || DEFAULT_ID} {...dataProps} {...props} data-nth="next" data-testid="server-side-target">
+    <Tag {...props} data-testid="server-side-target">
       {children}
     </Tag>
   );
 };
 
 /**
- * @example
- * ```tsx
- * <NextJsSSGThemeSwitcher />
- * ```
+ * @deprecated No longer need to add this component for avoiding FOUC.
+ *
+ * This component is deprecated and will be removed in the next major release. Use `ServerSideWrapper` instead.
+ * This component is kept here only to ensure no sudden breaking changes are introduced.
  */
 export const NextJsSSGThemeSwitcher = (props: NextJsSSRThemeSwitcherProps) => {
   return sharedServerComponentRenderer(props, "div");
 };
 
-/** For naming consistancy, clarity, and minimizing API updates */
+/**
+ * @deprecated No longer need to add this component for avoiding FOUC.
+ *
+ * This component is deprecated and will be removed in the next major release. Use `ServerSideWrapper` instead.
+ * This component is kept here only to ensure no sudden breaking changes are introduced.
+ */
 export { NextJsSSGThemeSwitcher as NextJsServerTarget };
 
 export interface ServerSideWrapperProps extends NextJsSSRThemeSwitcherProps {
@@ -94,16 +55,10 @@ export interface ServerSideWrapperProps extends NextJsSSRThemeSwitcherProps {
 }
 
 /**
- * Server side wrapper for Next.js to replace &#x60;html&#x60; tag
- * @example
- * ```tsx
- * <ServerSideWrapperProps lang="en">
- *  <body>
- *    <ThemeSwitcher />
- *    {children}
- *  </body>
- * </ServerSideWrapperProps>
- * ```
+ * @deprecated No longer need to add this component for avoiding FOUC.
+ *
+ * This component is deprecated and will be removed in the next major release. Use `ServerSideWrapper` instead.
+ * This component is kept here only to ensure no sudden breaking changes are introduced.
  */
 export const ServerSideWrapper = (props: ServerSideWrapperProps) => {
   return sharedServerComponentRenderer(props, "html");
