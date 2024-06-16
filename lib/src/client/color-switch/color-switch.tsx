e62bb77d68e9e115646b2646d56ef5e7b2a77e0e
Mayank <mayank.srmu@gmail.com>
2024-06-16 23:08:37 +0530

    Update props to match the old versions APIs exposed to users -- color-switch

--- lib/src/client/color-switch/color-switch.tsx
+++ lib/src/client/color-switch/color-switch.tsx
@@ -1,25 +1,28 @@
-import { HTMLProps, ReactNode } from "react";
+import { HTMLProps } from "react";
 import styles from "./color-switch.module.scss";
 
 export interface ColorSwitchProps extends HTMLProps<HTMLDivElement> {
-	children?: ReactNode;
+  /** Diameter of the color switch */
+  size?: number;
+  /** Skip system colorScheme while toggling */
+  skipSystem?: boolean;
 }
 
 /**
- * 
+ *
  *
  * @example
  * ```tsx
  * <ColorSwitch />
  * ```
- * 
+ *
  * @source - Source code
  */
 export const ColorSwitch = ({ children, ...props }: ColorSwitchProps) => {
   const className = [props.className, styles["color-switch"]].filter(Boolean).join(" ");
-	return (
-		<div {...props} className={className} data-testid="color-switch">
-			{children}
-		</div>
-	);
-}
+  return (
+    <div {...props} className={className} data-testid="color-switch">
+      {children}
+    </div>
+  );
+};
