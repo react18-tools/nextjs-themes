a2409ecc5ea9336b239fec88ca040d31a1d83f54
Mayank <mayank.srmu@gmail.com>
2024-06-16 23:11:25 +0530

    Update props to match the old versions APIs exposed to users -- force-color-scheme

--- lib/src/client/force-color-scheme/force-color-scheme.tsx
+++ lib/src/client/force-color-scheme/force-color-scheme.tsx
@@ -1,25 +1,24 @@
-import { HTMLProps, ReactNode } from "react";
-import styles from "./force-color-scheme.module.scss";
+import { HTMLProps } from "react";
+import { ColorSchemeType } from "../../types";
 
 export interface ForceColorSchemeProps extends HTMLProps<HTMLDivElement> {
-	children?: ReactNode;
+  colorScheme: ColorSchemeType;
 }
 
 /**
- * 
+ *
  *
  * @example
  * ```tsx
  * <ForceColorScheme />
  * ```
- * 
+ *
  * @source - Source code
  */
 export const ForceColorScheme = ({ children, ...props }: ForceColorSchemeProps) => {
-  const className = [props.className, styles["force-color-scheme"]].filter(Boolean).join(" ");
-	return (
-		<div {...props} className={className} data-testid="force-color-scheme">
-			{children}
-		</div>
-	);
-}
+  return (
+    <div {...props} data-testid="force-color-scheme">
+      {children}
+    </div>
+  );
+};
