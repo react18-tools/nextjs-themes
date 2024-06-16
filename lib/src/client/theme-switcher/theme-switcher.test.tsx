da439c1aae6e3bd45487841a8c31802908f31b13
Mayank <mayank.srmu@gmail.com>
2024-06-16 23:15:04 +0530

    Update props to match the old versions APIs exposed to users -- theme-switcher

--- lib/src/client/theme-switcher/theme-switcher.test.tsx
+++ lib/src/client/theme-switcher/theme-switcher.test.tsx
@@ -3,11 +3,11 @@ import { afterEach, describe, test } from "vitest";
 import { ThemeSwitcher } from "./theme-switcher";
 
 describe.concurrent("theme-switcher", () => {
-	afterEach(cleanup);
+  afterEach(cleanup);
 
-	test("Dummy test - test if renders without errors", ({ expect }) => {
-		const clx = "my-class";
-		render(<ThemeSwitcher className={clx} />);
-		expect(screen.getByTestId("theme-switcher").classList).toContain(clx);
-	});
+  test("Dummy test - test if renders without errors", ({ expect }) => {
+    const clx = "my-class";
+    render(<ThemeSwitcher />);
+    expect(screen.getByTestId("theme-switcher").classList).toContain(clx);
+  });
 });
--- lib/src/client/theme-switcher/theme-switcher.tsx
+++ lib/src/client/theme-switcher/theme-switcher.tsx
@@ -1,25 +1,24 @@
-import { HTMLProps, ReactNode } from "react";
-import styles from "./theme-switcher.module.scss";
+import { ColorSchemeType } from "../../types";
 
-export interface ThemeSwitcherProps extends HTMLProps<HTMLDivElement> {
-	children?: ReactNode;
+export interface ThemeSwitcherProps {
+  forcedTheme?: string;
+  forcedColorScheme?: ColorSchemeType;
+  targetSelector?: string;
+  themeTransition?: string;
+  /** provide styles object imported from CSS/SCSS modules, if you are using CSS/SCSS modules. */
+  styles?: Record<string, string>;
 }
 
 /**
- * 
+ *
  *
  * @example
  * ```tsx
  * <ThemeSwitcher />
  * ```
- * 
+ *
  * @source - Source code
  */
-export const ThemeSwitcher = ({ children, ...props }: ThemeSwitcherProps) => {
-  const className = [props.className, styles["theme-switcher"]].filter(Boolean).join(" ");
-	return (
-		<div {...props} className={className} data-testid="theme-switcher">
-			{children}
-		</div>
-	);
-}
+export const ThemeSwitcher = ({ ...props }: ThemeSwitcherProps) => {
+  return null;
+};
