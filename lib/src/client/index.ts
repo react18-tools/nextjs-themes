0b855476f935165a7cb1f24d6c90d6674f40a058
Mayank <mayank.srmu@gmail.com>
2024-06-17 08:55:43 +0530

    use theme hook

--- lib/src/client/index.ts
+++ lib/src/client/index.ts
@@ -8,6 +8,4 @@
 
 // client component exports
 export * from "./theme-switcher";
-export * from "./force-theme";
-export * from "./force-color-scheme";
 export * from "./color-switch";
--- lib/src/hooks/use-theme.ts
+++ lib/src/hooks/use-theme.ts
@@ -1,24 +1,63 @@
-import { useState } from "react";
+import { useEffect } from "react";
+import { useStore } from "../store";
+import { ResolveFunc } from "../client/theme-switcher/no-fouc";
+import { ColorSchemeType, ResolvedColorSchemeType } from "../types";
 
-export interface UseThemeOptions {
-  /** this is a dummy option */
-  dummy?: string;
+let resolveTheme: ResolveFunc;
+
+interface UseThemeYield {
+  theme: string;
+  darkTheme: string;
+  lightTheme: string;
+  colorSchemePref: ColorSchemeType;
+  systemColorScheme: ResolvedColorSchemeType;
+  resolvedColorScheme: ResolvedColorSchemeType;
+  resolvedTheme: string;
+  // actions
+  setTheme: (theme: string) => void;
+  setDarkTheme: (darkTheme: string) => void;
+  setLightTheme: (lightTheme: string) => void;
+  setThemeSet: (themeSet: { darkTheme: string; lightTheme: string }) => void;
+  setColorSchemePref: (colorSchemePref: ColorSchemeType) => void;
 }
 
 /**
- * 
+ *
  *
  * @example
  * ```tsx
  * const [] = useTheme(options);
  * ```
- * 
+ *
  * @source - Source code
  */
 
-export const useTheme = (options?: UseThemeOptions) => {
-  const [value, setValue] = useState(0);
+export const useTheme = (targetSelector?: string): UseThemeYield => {
+  const [state, setState] = useStore(targetSelector);
+  useEffect(() => {
+    resolveTheme = r;
+  }, []);
+
+  const [resolvedColorScheme, _, resolvedTheme] = resolveTheme(state);
+
+  /** helper */
+  const setter =
+    <T>(key: string) =>
+    (arg: T) =>
+      setState(state => ({ ...state, [key]: arg }));
+
   return {
-    value, setValue
-  }
-}
+    theme: state.t,
+    darkTheme: state.d,
+    lightTheme: state.l,
+    colorSchemePref: state.c,
+    systemColorScheme: state.s,
+    resolvedColorScheme,
+    resolvedTheme,
+    setTheme: setter<string>("t"),
+    setDarkTheme: setter<string>("d"),
+    setLightTheme: setter<string>("l"),
+    setThemeSet: ({ darkTheme: d, lightTheme: l }) => setState(state => ({ ...state, d, l })),
+    setColorSchemePref: setter<ColorSchemeType>("c"),
+  };
+};
