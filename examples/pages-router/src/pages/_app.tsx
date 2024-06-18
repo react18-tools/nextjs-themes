64e58c7f15ffe0eb6d493291f116e6f1055a36a6
Mayank <mayank.srmu@gmail.com>
2024-06-18 14:34:44 +0530

    Add linkComponents for examples

--- examples/pages-router/src/pages/_app.tsx
+++ examples/pages-router/src/pages/_app.tsx
@@ -5,6 +5,7 @@ import { Layout } from "@repo/shared/dist/server";
 import "../styles/global.css";
 import { Inter } from "next/font/google";
 import { Header } from "@repo/shared";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,7 @@ export default function App({ Component, pageProps }: _AppProps) {
     <div className={inter.className}>
       <ThemeSwitcher forcedColorScheme={colorScheme} forcedTheme={theme} />
       <Layout>
-        <Header />
+        <Header linkComponent={Link} />
         <Component {...pageProps} />
       </Layout>
     </div>
