1ab70ae7b8a4569e767bb55b2ea407b737e32fac
Mayank <mayank.srmu@gmail.com>
2024-06-18 15:25:37 +0530

    Add tests for the forced components

--- lib/src/client/force-color-scheme/force-color-scheme.test.tsx
+++ lib/src/client/force-color-scheme/force-color-scheme.test.tsx
@@ -1,13 +1,19 @@
-import { cleanup, render, screen } from "@testing-library/react";
+import { cleanup, render } from "@testing-library/react";
 import { afterEach, describe, test } from "vitest";
 import { ForceColorScheme } from "./force-color-scheme";
+import { DARK, DEFAULT_ID } from "../../constants";
+import { ThemeSwitcher } from "../theme-switcher";
+import { noFOUCScript } from "../theme-switcher/no-fouc";
+import { initialState } from "../../store";
 
+const key = `#${DEFAULT_ID}`;
 describe.concurrent("force-color-scheme", () => {
-	afterEach(cleanup);
-
-	test("Dummy test - test if renders without errors", ({ expect }) => {
-		const clx = "my-class";
-		render(<ForceColorScheme className={clx} />);
-		expect(screen.getByTestId("force-color-scheme").classList).toContain(clx);
-	});
+  afterEach(cleanup);
+  /** Test only the things that this component is responsible for - chanding state*/
+  test("Force theme with force color scheme", async ({ expect }) => {
+    noFOUCScript(key, initialState);
+    await render(<ThemeSwitcher />);
+    await render(<ForceColorScheme colorScheme={DARK} />);
+    expect(document.documentElement.classList).toContain(DARK);
+  });
 });
--- lib/src/client/force-theme/force-theme.test.tsx
+++ lib/src/client/force-theme/force-theme.test.tsx
@@ -1,13 +1,20 @@
-import { cleanup, render, screen } from "@testing-library/react";
+import { cleanup, render } from "@testing-library/react";
 import { afterEach, describe, test } from "vitest";
+import { DARK, DEFAULT_ID } from "../../constants";
+import { ThemeSwitcher } from "../theme-switcher";
+import { noFOUCScript } from "../theme-switcher/no-fouc";
+import { initialState } from "../../store";
 import { ForceTheme } from "./force-theme";
 
-describe.concurrent("force-theme", () => {
-	afterEach(cleanup);
-
-	test("Dummy test - test if renders without errors", ({ expect }) => {
-		const clx = "my-class";
-		render(<ForceTheme className={clx} />);
-		expect(screen.getByTestId("force-theme").classList).toContain(clx);
-	});
+const key = `#${DEFAULT_ID}`;
+describe.concurrent("force-color-scheme", () => {
+  afterEach(cleanup);
+  /** Test only the things that this component is responsible for - chanding state*/
+  test("Force theme with force color scheme", async ({ expect }) => {
+    noFOUCScript(key, initialState);
+    await render(<ThemeSwitcher />);
+    const myTheme = "myTheme";
+    await render(<ForceTheme theme={myTheme} />);
+    expect(document.documentElement.getAttribute("data-theme")).toBe(myTheme);
+  });
 });
