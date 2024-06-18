025e864d62bb044900f91b4b6a8e282c608bf6a1
Mayank <mayank.srmu@gmail.com>
2024-06-18 19:28:27 +0530

    rename to app-router

--- examples/app-router/src/app/layout.tsx
+++ examples/app-router/src/app/layout.tsx
@@ -5,6 +5,9 @@ import { Layout } from "@repo/shared/dist/server";
 import { GlobalLoader, Header } from "@repo/shared";
 import { Inter } from "next/font/google";
 import Link from "next/link";
+import { Cards, LandingPage } from "@repo/shared/dist/server";
+import { PageNavigatorCard, ThemeController } from "@repo/shared";
+import { ColorSwitch } from "nextjs-themes";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +20,13 @@ export default function RootLayout({ children }: { children: React.ReactNode }):
         <Layout>
           <Header linkComponent={Link} />
           {children}
+          <LandingPage title="Next.js Example">
+            <ColorSwitch className="center" />
+            <ThemeController />
+            <Cards>
+              <PageNavigatorCard LinkElement={Link} />{" "}
+            </Cards>
+          </LandingPage>
         </Layout>
         <GlobalLoader />
       </body>
---
+++ examples/app-router/src/app/page.tsx
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: "Nextjs Themes",
+};
+
+/** next.js landing page */
+export default function Page(): JSX.Element {
+  return <></>;
+}
