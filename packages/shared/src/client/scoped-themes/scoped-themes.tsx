63ad23f210eebae3a24903aa79b255787ebdef3c
Mayank <mayank.srmu@gmail.com>
2024-06-25 02:35:28 +0530

    fix styles

--- packages/shared/src/client/scoped-themes/scoped-themes.tsx
+++ packages/shared/src/client/scoped-themes/scoped-themes.tsx
@@ -6,7 +6,9 @@ export function ScopedThemes() {
   const id = "scoped-themes";
   const targetSelector = `#${id}`;
   return (
-    <div id={id} style={{ background: `var(--bg-color)`, color: `var(--text-color)` }}>
+    <div
+      id={id}
+      style={{ background: `var(--bg-color)`, color: `var(--text-color)`, textAlign: "center" }}>
       <h1>
         Apply themes locally using <code>targetSelector</code>
       </h1>
