c2ca688410b805a608bd7d0e02b94cb1b76aa774
Mayank <mayank.srmu@gmail.com>
2024-06-24 21:21:28 +0530

    Update docs title

--- typedoc.config.js
+++ typedoc.config.js
@@ -1,7 +1,7 @@
 /** @type {import('typedoc').TypeDocOptions} */
 module.exports = {
   tsconfig: "tsconfig.docs.json",
-  name: "React18 Loaders",
+  name: "Nextjs Themes",
   entryPoints: ["./lib/src"],
   exclude: ["**/*.test.tsx", "**/index.ts", "**/declaration.d.ts"],
   entryPointStrategy: "Expand",
