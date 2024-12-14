2860be9a3722562c90732ef7dd0a5aa3abd528b3
Mayank <mayank.srmu@gmail.com>
2024-12-14 07:24:17 +0530

    remove deprecated component form examples

--- examples/tailwind/src/app/layout.tsx
+++ examples/tailwind/src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeSwitcher } from "nextjs-themes";
-import { NextJsSSGThemeSwitcher } from "nextjs-themes/server";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +15,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={inter.className}>
-        <NextJsSSGThemeSwitcher />
         <ThemeSwitcher />
         {children}
       </body>
