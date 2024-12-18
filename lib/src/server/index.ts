a1a0c70bfb48b4e223e3a95a49fc012e6fb92838
Mayank <mayank.srmu@gmail.com>
2024-12-18 21:19:57 +0530

    clean up

--- lib/src/server/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Server components and client components need to be exported from separate files as
- * directive on top of the file from which component is imported takes effect.
- * i.e., server component re-exported from file with "use client" will behave as client component
- */
-
-// server component exports
-export * from "./nextjs";
